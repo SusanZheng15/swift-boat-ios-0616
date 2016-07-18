36f421676a951cfab9b00a535d4e1c6854911408
SusanZheng15 <zheng.susan12@gmail.com>
2016-07-18 16:43:07 -0400

    Done

--- swift-boat/AppDelegate.swift
+++ swift-boat/AppDelegate.swift
@@ -3,12 +3,14 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
 
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
         // Override point for customization after application launch.
         
         /**
---
+++ swift-boat/Boat.swift
@@ -0,0 +1,31 @@
+//
+//  Boat.swift
+//  swift-boat
+//
+//  Created by Flatiron School on 7/18/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class Boat
+{
+    var name: String
+    var sailors: [String]
+    var maxSpeedKnots: Double
+    var speedKnots = 0
+
+    init(name: String, sailors: [String], maxSpeedKnots: Double)
+    {
+        self.name = name
+        self.sailors = sailors
+        self.maxSpeedKnots = maxSpeedKnots
+        
+    }
+
+    convenience init(name: String, maxSpeedKnots: Double)
+    {
+        self.init(name: name, sailors: [], maxSpeedKnots: maxSpeedKnots)
+    }
+    
+}
